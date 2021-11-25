862d066e939131753acfb9ee7320de3c0622d04f
GATTA Houdjath <houdjath23gatta@gmail.com>
2021-11-25 18:24:14 +0100

    creation de l'interface calculatrice(#5)

--- app/src/main/java/Houdjath/demo/CalculatriceActivity.java
+++ app/src/main/java/Houdjath/demo/CalculatriceActivity.java
@@ -3,6 +3,7 @@ package Houdjath.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CalculatriceActivity extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class CalculatriceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculatrice);
+    }
+
+    public void percentEvent(View view) {
+    }
+
+    public void buttonEvent(View view) {
+    }
+
+    public void equalEvent(View view) {
+    }
+
+    public void operationEvent(View view) {
+    }
+
+    public void acEvent(View view) {
     }
 }
